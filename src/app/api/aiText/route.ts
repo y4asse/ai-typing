5a2c04fc3028f5c502e4f68a37e499c5f61e8c2f
y4asse <yasse0218@icloud.com>
2024-03-17 00:14:06 +0900

    ビルドエラー修正

--- src/app/api/aiText/route.ts
+++ src/app/api/aiText/route.ts
@@ -6,42 +6,43 @@ import OpenAI from 'openai'
 export const runtime = 'edge'
 
 export async function POST(request: Request) {
-  try {
-    const body = await request.json()
-    const { thema } = body
-    const openai = new OpenAI()
+  // try {
+  //   const body = await request.json()
+  //   const { thema } = body
+  //   const openai = new OpenAI()
 
-    const completion = await openai.chat.completions.create({
-      messages: [
-        {
-          role: 'system',
-          content:
-            'あなたはユーザからのテーマに基づいて、タイピング用文章を作成するアシスタントです。ユーザからのテーマに関連した40文字程度の文章を5つ生成して次のようなJSON形式で返してください。{ 1: string, 2: string, 3: string, 4: string, 5: string }'
-        },
-        { role: 'user', content: thema }
-      ],
-      model: 'gpt-4-turbo-preview',
-      response_format: { type: 'json_object' }
-    })
+  //   const completion = await openai.chat.completions.create({
+  //     messages: [
+  //       {
+  //         role: 'system',
+  //         content:
+  //           'あなたはユーザからのテーマに基づいて、タイピング用文章を作成するアシスタントです。ユーザからのテーマに関連した40文字程度の文章を5つ生成して次のようなJSON形式で返してください。{ 1: string, 2: string, 3: string, 4: string, 5: string }'
+  //       },
+  //       { role: 'user', content: thema }
+  //     ],
+  //     model: 'gpt-4-turbo-preview',
+  //     response_format: { type: 'json_object' }
+  //   })
 
-    if (!completion.choices[0].message.content) {
-      throw new Error('[OpenAI] completion.choices[0].message.content is not found.')
-    }
-    const content: {
-      [key: number]: string
-    } = JSON.parse(completion.choices[0].message.content)
+  //   if (!completion.choices[0].message.content) {
+  //     throw new Error('[OpenAI] completion.choices[0].message.content is not found.')
+  //   }
+  //   const content: {
+  //     [key: number]: string
+  //   } = JSON.parse(completion.choices[0].message.content)
 
-    let data: { text: string; hiragana: string }[] = []
-    for (const key in content) {
-      const text = content[key]
-      const tirmedText = trimText(text)
-      const katakana = await convertToKatakana(tirmedText)
-      const hiragana = kanaToHira(katakana)
-      data.push({ text: tirmedText, hiragana })
-    }
-    return Response.json({ data })
-  } catch (error) {
-    console.error(error)
-    return Response.json({ message: 'Internal Server Error' }, { status: 500 })
-  }
+  //   let data: { text: string; hiragana: string }[] = []
+  //   for (const key in content) {
+  //     const text = content[key]
+  //     const tirmedText = trimText(text)
+  //     const katakana = await convertToKatakana(tirmedText)
+  //     const hiragana = kanaToHira(katakana)
+  //     data.push({ text: tirmedText, hiragana })
+  //   }
+  //   return Response.json({ data })
+  // } catch (error) {
+  //   console.error(error)
+  //   return Response.json({ message: 'Internal Server Error' }, { status: 500 })
+  // }
+  return Response.json({ message: 'hello' })
 }
