f2026a7e22b13be20918c7d303225c1ee97c4fc2
y4asse <yasse0218@icloud.com>
2024-03-23 00:21:20 +0900

    レイアウト修正

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -7,15 +7,21 @@ const Footer = () => {
     <div className="bg-gray-100 dark:bg-gray-900 py-10 text-gray-500 dark:text-gray-300">
       <div className="max-w-[1000px] px-3 mx-auto">
         <div className="flex flex-col gap-2 h-full">
-          <Link href="/privacy" className="">
-            プライバシーポリシー
-          </Link>
-          <Link href="/about" className="">
-            このサイトについて
-          </Link>
-          <a href="https://twitter.com/y4isse" target="_blank">
-            開発者
-          </a>
+          <div>
+            <Link href="/privacy" className="">
+              プライバシーポリシー
+            </Link>
+          </div>
+          <div>
+            <Link href="/about" className="">
+              このサイトについて
+            </Link>
+          </div>
+          <div>
+            <a href="https://twitter.com/y4isse" target="_blank">
+              開発者
+            </a>
+          </div>
         </div>
       </div>
       <div className=" text-center mt-10">
