945ca18550281847ac9bcaacd74e738278cbbe57
y4asse <yasse0218@icloud.com>
2024-03-25 00:47:17 +0900

    シングルクォート修正

--- src/app/(en)/en/about/play/page.tsx
+++ src/app/(en)/en/about/play/page.tsx
@@ -5,14 +5,14 @@ const Page = () => {
     <div className="container min-h-screen">
       <div className="pb-20">
         <h1 className="text-4xl font-bold">How to Play</h1>
-        <p className="mt-5 text-gray-500">Here's how to play AI Typing:</p>
+        <p className="mt-5 text-gray-500">Here&apos;s how to play AI Typing:</p>
         <div className="mt-10">
           <h2 className="text-3xl">○Rules</h2>
           <p>
-            ・As you type the displayed text, you'll earn points based on the time taken, the number of words typed, and
-            the number of typing errors. The timer starts when you type the first character.
+            ・As you type the displayed text, you&apos;ll earn points based on the time taken, the number of words
+            typed, and the number of typing errors. The timer starts when you type the first character.
           </p>
-          <p>・You'll be presented with 5 texts to type.</p>
+          <p>・You&apos;ll be presented with 5 texts to type.</p>
           <p>
             ・This game supports only Japanese and English languages. Please note that using characters from other
             languages may cause the game to malfunction.
